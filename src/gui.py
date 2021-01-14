ac63577ccaeaec8726ed4151df7c2d44ceb77b1f
Lucas MIA <52677245+LucasMIA@users.noreply.github.com>
2021-01-13 19:32:52 -0500

    version bump

--- src/gui.py
+++ src/gui.py
@@ -22,7 +22,7 @@ from operator import itemgetter
 addon_path = dirname(__file__)
 import platform
 
-verNumber = "1.2.0"
+verNumber = "1.2.1"
 
 class JSGui(QScrollArea):
     def __init__(self, mw, colArray, MigakuModel, reboot, CSSJSHandler, UEManager):
